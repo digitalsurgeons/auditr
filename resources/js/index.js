c848c6a82a122dd05ebc3a5985d39ec7f4958239
Dylan Baker <dylan.baker@digitalsurgeons.com>
2017-11-03 11:55:46 -0400

    Refactor and style page speed insights

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,45 +1,113 @@
+function displayRecommendation(recommendation, index) {
+  if (recommendation.summary) {
+    let summary = recommendation.summary
+
+    if (summary.args) {
+      summary.args.forEach(function (arg) {
+        // populate format string
+        if (arg.type == 'HYPERLINK') {
+          summary.format = summary.format.replace('{{BEGIN_LINK}}', `<a href="${arg.value}">&nbsp;`)
+          summary.format = summary.format.replace('{{END_LINK}}',`</a>`)
+        } else {
+          summary.format = summary.format.replace(`{{${arg.key}}}`, arg.value)
+        }
+      })
+    }
+
+    return `
+      <dt class="Section__list-title${index % 2 == 0 ? " Section__list-item--gray" : ''}">
+        ${recommendation.localizedRuleName}
+      </dt>
+      <dd class="Section__list-item${index % 2 == 0 ? " Section__list-item--gray" : ''}">
+        ${recommendation.summary.format}
+      </dd>
+    `
+  }
+}
+
+
 function showInsights(resp) {
-  $container = $('<div>')
-  $pageStats = `
-    <h3>Speed Score: ${resp.ruleGroups.SPEED.score}</h3>
-    <table class="Section__table">
-      <tbody>
-        <tr>
-          <td class="Section__table-cell">Number of Resources</td><td class="Section__table-cell">${resp.pageStats.numberResources}</td>
-        </tr>
-        <tr>
-          <td class="Section__table-cell">Number of Hosts</td><td class="Section__table-cell">${resp.pageStats.numberHosts}</td>
-        </tr>
-        <tr>
-          <td class="Section__table-cell">Total Request Bytes</td><td class="Section__table-cell">${resp.pageStats.totalRequestBytes}</td>
-        </tr>
-        <tr>
-          <td class="Section__table-cell">Number Static Resources</td><td class="Section__table-cell">${resp.pageStats.numberStaticResources}</td>
-        </tr>
-        <tr>
-          <td class="Section__table-cell">HTML Response Bytes</td><td class="Section__table-cell">${resp.pageStats.htmlResponseBytes}</td>
-        </tr>
-        <tr>
-          <td class="Section__table-cell">CSS Response Bytes</td><td class="Section__table-cell">${resp.pageStats.cssResponseBytes}</td>
-        </tr>
-        <tr>
-          <td class="Section__table-cell">Image Response Bytes</td><td class="Section__table-cell">${resp.pageStats.imageResponseBytes}</td>
-        </tr>
-        <tr>
-          <td class="Section__table-cell">JavaScript Response Bytes</td><td class="Section__table-cell">${resp.pageStats.javascriptResponseBytes}</td>
-        </tr>
-        <tr>
-          <td class="Section__table-cell">Number of JS Resources</td><td class="Section__table-cell">${resp.pageStats.numberJsResources}</td>
-        </tr>
-        <tr>
-          <td class="Section__table-cell">Number of CSS Resources</td><td class="Section__table-cell">${resp.pageStats.numberCssResources}</td>
-        </tr>
-      </tbody>
-    </table>
-  `
-  $container.append($pageStats)
+  // show score
+  let rating
+  if (resp.ruleGroups.SPEED.score < 65) {
+    rating = 'poor'
+  } else if (resp.ruleGroups.SPEED.score < 85) {
+    rating = 'needsWork'
+  } else {
+    rating = 'good'
+  }
+
   $('[data-insights]').empty()
-  $('[data-insights]').append($container)
+  $('[data-insights]').append(`
+      <div class="Speed-score Speed-score--${rating}">
+        <p class="Speed-score__score">${resp.ruleGroups.SPEED.score} / 100</p>
+      </div>
+    `
+  )
+
+  // dynamically make the score circular
+  $('.Speed-score__score').height($('.Speed-score__score').width());
+  $('.Speed-score__score').css('line-height', $('.Speed-score__score').width() + "px")
+
+  // initialize lists
+  // is there a better way to initialize multiple variables to the same value
+  // without having them reference each other?
+  let recommendations = $('<dl>', {
+    class: 'Section__list Section__list--defs'
+  })
+
+  let optimizationsFound = $('<dl>', {
+    class: 'Section__list Section__list--defs'
+  })
+
+  let stats = $('<dl>', {
+    class: 'Section__list Section__list--defs'
+  })
+
+  // create recommendations lists
+  let rules = resp.formattedResults.ruleResults
+
+  let filters = [
+    {
+      // if the rule impact is 0, this is something you've already done
+      f: rule => rules[rule].summary != undefined && rules[rule].ruleImpact === 0.0,
+      $o: optimizationsFound
+    },
+    {
+      f: rule => rules[rule].summary != undefined && rules[rule].ruleImpact > 0.0,
+      $o: recommendations
+    }
+  ]
+
+  filters.forEach(function (filter) {
+    $(filter.$o).append(
+      Object.keys(rules).filter(filter.f).map(function (key, index) {
+          return displayRecommendation(rules[key], index)
+      })
+    )
+  })
+
+  $('[data-insights]').append('<h3 class="Section__subheader">Opimizations Found</h3>')
+  $('[data-insights]').append(optimizationsFound)
+
+  $('[data-insights]').append('<h3 class="Section__subheader">Recommendations</h3>')
+  $('[data-insights]').append(recommendations)
+
+  Object.keys(resp.pageStats).forEach(function (key, index) {
+    // camelCase => Title Case
+    let title = key
+      .replace(/([A-Z])/g, ' $1')
+      .replace(/^./, str => str.toUpperCase())
+
+    $(stats).append(`
+        <dt class="Section__list-title${index % 2 == 0 ? " Section__list-item--gray" : ''}">${title}</dt>
+        <dd class="Section__list-item${index % 2 == 0 ? " Section__list-item--gray" : ''}">${resp.pageStats[key]}</dd>
+      `
+    )
+  })
+
+  $('[data-insights]').append('<h3 class="Section__subheader">Statistics</h3>')
+  $('[data-insights]').append(stats)
 }
 
 $(document).ready(function() {
@@ -56,12 +124,12 @@ $(document).ready(function() {
     if (key) {
       $.ajax({
         method: 'GET',
-        url: "https://www.googleapis.com/pagespeedonline/v2/runPagespeed",
+        url: 'https://www.googleapis.com/pagespeedonline/v2/runPagespeed',
         dataType: 'json',
         data: {
           key: key,
           url: siteUrl,
-          fields: "pageStats,ruleGroups",
+          fields: 'pageStats,formattedResults,ruleGroups',
           strategy: 'desktop'
         },
         success: resp => {
@@ -72,7 +140,12 @@ $(document).ready(function() {
           var errors = resp.responseJSON.error.errors
 
           for (var i = 0; i < errors.length; i++) {
-            $('[data-insights]').append(`<p class="Section__error">${errors[i].message} (${errors[i].reason})</p>`)
+            $('[data-insights]').append(`
+                <p class="Section__error">
+                  Error: ${errors[i].message} (${errors[i].reason})
+                </p>
+              `
+            )
           }
         }
       })
