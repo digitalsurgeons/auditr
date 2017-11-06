746b35a730cc023ee6580d83afae453a7b3835e2
Dylan Baker <dylan.baker@digitalsurgeons.com>
2017-11-06 09:27:44 -0500

    Use different pdf library

--- controllers/AuditrController.php
+++ controllers/AuditrController.php
@@ -2,15 +2,23 @@
 
 namespace Craft;
 
+
 class AuditrController extends BaseController
 {
     protected $allowAnonymous = [];
 
+    public function __construct()
+    {
+        require __DIR__ . '/../vendor/autoload.php';
+    }    
+
     public function actionConvert()
     {
         $html = $this->renderTemplate('auditr/export', array(), true);
-        $this->_generatePDF($html);
-        $this->redirect('auditr');
+        $dompdf = new \Dompdf\Dompdf();
+        $dompdf->loadHtml($html);
+        $dompdf->render();
+        $dompdf->stream('Auditr.pdf');
     }
 
     public function actionDownload()
