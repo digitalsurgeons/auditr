41ae067aecd1eb7d8951a3a3be31ccb487610bd6
Dylan Baker <dylan.baker@digitalsurgeons.com>
2017-11-06 09:32:25 -0500

    Simplify pdf buttons

--- controllers/AuditrController.php
+++ controllers/AuditrController.php
@@ -12,28 +12,12 @@ class AuditrController extends BaseController
         require __DIR__ . '/../vendor/autoload.php';
     }    
 
-    public function actionConvert()
+    public function actionDownload()
     {
         $html = $this->renderTemplate('auditr/export', array(), true);
         $dompdf = new \Dompdf\Dompdf();
         $dompdf->loadHtml($html);
         $dompdf->render();
         $dompdf->stream('Auditr.pdf');
-    }
-
-    public function actionDownload()
-    {
-        $file = IOHelper::getFileContents(__DIR__ . '/../resources/Auditr.pdf');
-        HeaderHelper::setHeader([
-            'Content-Type' => 'application/pdf',
-            'Content-Disposition' => 'inline; filename=Auditr.pdf'
-        ]);
-        echo $file;
-    }
-
-    private function _generatePDF($html)
-    {
-        craft()->tasks->createTask('Auditr','Generate PDF', ['html' => $html]);
-        craft()->userSession->setNotice(Craft::t('Auditr: PDF generation started.'));
     }
 }
